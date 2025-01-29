d0d5b07ed31230ebe77ec0ea2cbbfaff2a293170
AlexGitHubP <47295297+AlexGitHubP@users.noreply.github.com>
2025-01-29 06:59:53 +0200

    Update Media.php

--- src/Models/Media.php
+++ src/Models/Media.php
@@ -6,5 +6,42 @@ use Illuminate\Database\Eloquent\Model;
 
 class Media extends Model
 {
+    static function getMainImage($owner, $ownerID, $isMain = true){
+        $query = Media::where('owner', '=', $owner)
+                    ->where('owner_id', '=', $ownerID);
+        if ($isMain) {
+            $query->where('main', '=', 1);
+        }
+        $media = $query->first();
+
+        $mediaPath = !empty($media) ? '/'.$media->folder.'/'.$media->file : asset('/img/noimg.png');
+
+        return $mediaPath;
+    }
+
+    static function getImagesWithoutMain($owner, $ownerID){
+        $media = Media::where('owner',    '=', $owner)
+                ->where('owner_id', '=', $ownerID)
+                ->where('main', '=', 0)
+                ->orderBy('ordering_owner', 'ASC')
+                ->get();
+                
+        $media->map(function($mediaElement){
+            $mediaElement->file = (!empty($mediaElement->file) || $mediaElement->file!=null) ? $mediaElement->folder.'/'.$mediaElement->file : asset('img/noimg.png');
+        });
+        
+        return $media;
+    }
+    static function getAllImages($owner, $ownerID){
+        $media = Media::where('owner',  '=', $owner)
+                    ->where('owner_id', '=', $ownerID)
+                    ->get();
+        
+        $media->map(function($mediaElement){
+            $mediaElement->file = (!empty($mediaElement->file) || $mediaElement->file!=null) ? $mediaElement->folder.'/'.$mediaElement->file : asset('img/noimg.png');
+        });
+
+        return $media;
+    }
     
 }
